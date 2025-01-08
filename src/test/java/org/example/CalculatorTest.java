dd47f88df6fdbaa1dee7edebb3b1578ef5e5c0ed
Yaroslav Rymarchuk <yaroslav.rymarchuk.qa@ukr.net>
2025-01-08 13:44:24 +0300

    Through browserStack

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,9 +1,8 @@
 package org.example;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.By;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,31 +11,32 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CalculatorTest {
+
     private AndroidDriver driver;
+
     @BeforeClass
     public void setUp() throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "15.0");
-        caps.setCapability("deviceName", "emulator-5554");
-        caps.setCapability("appPackage", "com.google.android.calculator");
-        caps.setCapability("appActivity", ".Calculator");
-        caps.setCapability("automationName", "UiAutomator2");
+        UiAutomator2Options options = new UiAutomator2Options();
 
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        options.setCapability("browserstack.user", "bsuser_PZcs6M");
+        options.setCapability("browserstack.key", "RszxAFsyyrrmAHcV639L");
+        options.setCapability("app", "bs://2ffeeb8c67c2e4857054ea125a5b09fc6b2e6f58");
+
+        options.setDeviceName("OnePlus 7");
+        options.setPlatformVersion("9.0");
+        options.setAutomationName("UiAutomator2");
+
+        driver = new AndroidDriver(new URL("http://hub.browserstack.com/wd/hub"), options);
     }
 
     @Test
     public void testAddition() {
-        driver.findElement(By.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("op_add")).click();
+        driver.findElement(AppiumBy.id("digit_2")).click();
+        driver.findElement(AppiumBy.id("eq")).click();
 
-        driver.findElement(By.id("op_add")).click();
-
-        driver.findElement(By.id("digit_2")).click();
-
-        driver.findElement(By.id("eq")).click();
-
-        String result = driver.findElement(By.id("result_final")).getText();
+        String result = driver.findElement(AppiumBy.id("result_final")).getText();
         System.out.println("Result: " + result);
 
         assert result.equals("3");
